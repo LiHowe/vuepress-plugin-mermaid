1769a85943fd29fc83562364ac03eaff3cd56099
LiHowe <howe.personal@gmail.com>
2021-11-18 17:27:21 +0800

    update: change build config

--- lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare const _default: () => {
-    name: string;
-    clientAppEnhanceFiles: string;
-    extendsMarkdown: (md: any) => void;
-};
-export default _default;
--- src/node/index.ts
+++ src/node/index.ts
@@ -3,7 +3,7 @@ import { path } from '@vuepress/utils'
 
 export default  () => ({
   name: 'vuepress-plugin-markdown-mermaid',
-  clientAppEnhanceFiles: path.resolve(__dirname, './enhance.ts'),
+  clientAppEnhanceFiles: path.resolve(__dirname, './enhance.js'),
   extendsMarkdown: (md: any) => {
     md.use(MermaidPlugin)
   }
