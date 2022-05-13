de9ab0720e41d044f7e542c9f193cbfcad0a6977
LiHowe <howe.personal@gmail.com>
2022-05-13 13:44:20 +0800

    chore: remove the console warning

--- src/client/enhance.ts
+++ src/client/enhance.ts
@@ -4,6 +4,5 @@ import Mermaid from './Mermaid'
 export default defineClientAppEnhance(({ app }) => {
   if (!__VUEPRESS_SSR__) {
     customElements.define('h-mermaid', Mermaid)
-    app.config.compilerOptions.isCustomElement = tag => tag.startsWith('h-')
   }
 })
